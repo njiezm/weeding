aad5b1eddce61269fe250a23d1f404cd6f51357f
Njie <njie@wak-studio.com>
2025-12-25 12:29:20 -0400

    j-1

--- resources/views/pages/urne.blade.php
+++ resources/views/pages/urne.blade.php
@@ -123,7 +123,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         // 🔴 REMPLACE PAR TON LIEN PAYPAL.ME
-        const paypalLink = "https://paypal.me/TONLIENPAYPAL/" + montant;
+        const paypalLink = "https://www.paypal.com/paypalme/MaevaAntiste" + montant; 
 
         window.location.href = paypalLink;
     });
